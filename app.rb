7d89dfa594058eda7361a8364499f21c4d462536
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-11-01 15:12:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,3 +6,16 @@ class App < Sinatra::Base
 
 
 end
+
+
+App
+  GET /hello
+    sends a 200 status code (FAILED - 1)
+    renders a template called "hello.erb" (FAILED - 2)
+  GET /goodbye
+    sends a 200 status code (FAILED - 3)
+    renders a template called "goodbye.erb" (FAILED - 4)
+  GET /date
+    sends a 200 status code (FAILED - 5)
+    renders a template called "date.erb" (FAILED - 6)
+    includes the current date and time (FAILED - 7)
